e92301d1577decbcb021dc86f0b42f88b546b2ec
caiomiyaji <c.hidekimiyaji@gmail.com>
2022-11-03 20:28:12 -0300

    test carousel smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 import {boats} from "./boats.js";
 
+const [...shuffleBoats] = boats;
+
+shuffleBoats.sort(() => {
+    return Math.random() - 0.5
+});
+
+const carouselBoats = shuffleBoats.slice(0, 10);
+
 // Functions
 
+//Add boat to carousel
 const insertBoat = (boatCards, cardSample, currentBoat) => {
     const newCard = cardSample.cloneNode(true);
     newCard.classList.remove('hide')
+    newCard.classList.add('visible-card')
     const cardImg = newCard.querySelector('img').src = currentBoat.pictures;
     const cardTitle = newCard.querySelector('h3').textContent = currentBoat.name;
     const cardLocation = newCard.querySelector('.card-location > p').textContent = currentBoat.location;
@@ -16,14 +26,50 @@ const insertBoat = (boatCards, cardSample, currentBoat) => {
     boatCards.append(newCard);
 }
 
+//Move the carousel
+const moveCarousel = (currentButton, container, containerWidth) => {
+    const nextButton = currentButton.classList.contains('right-button');
+
+    if(nextButton){
+        container.scrollTo({
+            top: 0,
+            left: container.scrollLeft + containerWidth,
+            behavior: "smooth"
+        })
+    }else{
+        container.scrollTo({
+            top: 0,
+            left: container.scrollLeft - containerWidth,
+            behavior: "smooth"
+        })
+    }
+}
+
+
+// events
+
+//When page loads
 window.addEventListener('load', (e) => {
     const boatsCards = document.querySelector('.boats-cards');
     const cardSample = document.querySelector('.card');
 
-    boats.forEach((boat) => {
+    carouselBoats.forEach((boat) => {
         insertBoat(boatsCards, cardSample, boat);
     })
 
+})
+
+//Carousel Next-Prev
+const carouselButtons = document.querySelectorAll('.cards-buttons button');
+
+carouselButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const boatContainer = document.querySelector('.boats-cards');
+        const containerRect = boatContainer.getBoundingClientRect();
+        const containerWidth = containerRect.width;
+
+        moveCarousel(button, boatContainer, containerWidth,)
+    })
 })
 
 
